d5a657cb140b7d73b31d98f259f5bee6586d73df
Fara Woolf <fbwoolf@gmail.com>
2017-06-28 11:01:30 -0500

    Setup api routes

--- public/js/main.js
+++ public/js/main.js
@@ -2,3 +2,14 @@
 /* global $ tripModule */
 
 $(tripModule.load);
+
+$.ajax({
+  method: 'GET',
+  url: '/api/hotels'
+})
+.then(function (responseData) {
+  console.log(responseData);
+})
+.catch(function (errorObj) {
+  console.log(errorObj);
+});
--- routes/index.js
+++ routes/index.js
@@ -20,4 +20,29 @@ router.get('/', function(req, res, next) {
   .catch(next);
 });
 
+// AJAX routes
+router.get('/api/hotels', function(req, res, next) {
+    Hotel.findAll( )
+      .then(hotels => {
+        res.json(hotels)
+      })
+      .catch(next);
+});
+
+router.get('/api/restaurants', function(req, res, next) {
+    Restaurant.findAll( )
+      .then(restaurants => {
+        res.json(restaurants)
+      })
+      .catch(next);
+});
+
+router.get('/api/activities', function(req, res, next) {
+    Activity.findAll( )
+      .then(activities => {
+        res.json(activities)
+      })
+      .catch(next);
+});
+
 module.exports = router;
